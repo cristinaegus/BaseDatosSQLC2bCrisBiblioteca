b9dc5c582d347c281ec6ea8e151e444aab4f8fbf
unknown <egusquizacristina@gmail.com>
2025-06-06 14:07:48 +0200

    modificado puerto en iniciar_api2_8010.bat

--- iniciar_api2_8010.bat
+++ iniciar_api2_8010.bat
@@ -1,5 +1,5 @@
 @echo off
-REM Script para arrancar FastAPI con main_api2.py en el puerto 8010
+REM Script para arrancar FastAPI con main_api2.py en el puerto 8020
 cd /d %~dp0
-uvicorn biblioteca.main_api2:app --reload --port 8010
+uvicorn biblioteca.main_api2:app --reload --port 8020
 pause
